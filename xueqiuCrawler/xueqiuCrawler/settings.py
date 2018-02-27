368ff17d5939db05c17070ee6491e4645b6508a0
yinzongming <yinzongming@gmail.com>
2018-02-27 18:50:44 +0800

    add redis model, but get a bug: 403 code

--- xueqiuCrawler/xueqiuCrawler/settings.py
+++ xueqiuCrawler/xueqiuCrawler/settings.py
@@ -20,6 +20,26 @@ MONGODB_POST = 27017
 MONGODB_DBNAME = "xueqiu"
 MONGODB_COLLECTION = "xueqiu"
 
+############### set redis configuration
+#
+SCHEDULER = 'xueqiuCrawler.scrapy_redis.scheduler.Scheduler'
+SCHEDULER_PERSIST = True
+# SCHEDULER_QUEUE_CLASS = 'xueqiuCrawler.scrapy_redis.queue.SpiderPriorityQueue'
+SCHEDULER_QUEUE_CLASS = 'xueqiuCrawler.scrapy_redis.queue.SpiderSimpleQueue'
+
+# 种子队列的信息
+REDIE_URL = None
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+
+# 去重队列的信息
+FILTER_URL = None
+FILTER_HOST = 'localhost'
+FILTER_PORT = 6379
+FILTER_DB = 0
+REDIS_QUEUE_NAME = 'Xueqiu'   # 如果不设置或者设置为None，则使用默认的，每个spider使用不同的去重队列和种子队列。如果设置了，则不同spider共用去重队列和种子队列
+#
+##################################
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'xueqiuCrawler (+http://www.yourdomain.com)'
--- xueqiuCrawler/xueqiuCrawler/spiders/xueqiu.py
+++ xueqiuCrawler/xueqiuCrawler/spiders/xueqiu.py
@@ -3,15 +3,15 @@ import scrapy
 import json
 from collections import deque
 from xueqiuCrawler.items import XueqiucrawlerItem
+from xueqiuCrawler.scrapy_redis.spiders import RedisSpider
 
-class XueqiuSpider(scrapy.Spider):
+class XueqiuSpider(RedisSpider):
     name = 'xueqiu'
     allowed_domains = ['xueqiu.com']
-    # start_urls = ['https://xueqiu.com/friendships/groups/members.json?uid=5762889842&page=1&gid=0']
-    start_urls = ['https://xueqiu.com/friendships/groups/members.json?uid=7675636633&page=1&gid=0']
-
+    start_urls = ['https://xueqiu.com/friendships/groups/members.json?uid=5762889842&page=1&gid=0']
+    first_user_id = 5762889842
     id_queue = deque() # 层次遍历所有的用户
-
+    # 在关注页面用的header
     my_header = {
         "Host": "xueqiu.com",
         "Connection": "keep-alive",
@@ -21,16 +21,26 @@ class XueqiuSpider(scrapy.Spider):
         "Referer": "https://xueqiu.com/u/5762889842",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-        "Cookie": "device_id=288387fa2efe85a6e006edc54a5eda57; s=fp1157htg6; bid=b7785bba313ecf158609498a2d9f49bf_jcjuxw50; __utmz=1.1516242744.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); aliyungf_tc=AQAAAI1I/Rz9mw0AcSk2t/5QRvv9hlDR; Hm_lvt_1db88642e346389874251b5a1eded6e3=1517477327,1517973487,1519619536; remember=1; remember.sig=K4F3faYzmVuqC0iXIERCQf55g2Y; xq_a_token=aae1db46abe1e1ed71f6fea88fde2ed306c64063; xq_a_token.sig=rHnC-h2pvBo_iFv1RBKNQsaUc6U; xq_r_token=8ef08c78aefc61aa9394f163640e68c81d338b99; xq_r_token.sig=tDqW3wGLsJBSypal5tKrNYGkbAw; xq_is_login=1; xq_is_login.sig=J3LxgPVPUzbBg3Kee_PquUfih7Q; u=6507446955; u.sig=cSuuCuFU_r2EmTKvwOFMewzKcyI; __utma=1.1764713070.1516242744.1517477332.1519619554.5; __utmc=1; __utmt=1; __utmb=1.1.10.1519619554; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1519619563",
+        "Cookie": "device_id=288387fa2efe85a6e006edc54a5eda57; s=fp1157htg6; bid=b7785bba313ecf158609498a2d9f49bf_jcjuxw50; __utmz=1.1516242744.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); remember=1; remember.sig=K4F3faYzmVuqC0iXIERCQf55g2Y; xq_a_token=aae1db46abe1e1ed71f6fea88fde2ed306c64063; xq_a_token.sig=rHnC-h2pvBo_iFv1RBKNQsaUc6U; xq_r_token=8ef08c78aefc61aa9394f163640e68c81d338b99; xq_r_token.sig=tDqW3wGLsJBSypal5tKrNYGkbAw; xq_is_login=1; xq_is_login.sig=J3LxgPVPUzbBg3Kee_PquUfih7Q; u=6507446955; u.sig=cSuuCuFU_r2EmTKvwOFMewzKcyI; aliyungf_tc=AQAAAPnKBHOEaw4AomIYdLQ43IMpgjBu; __utmc=1; Hm_lvt_1db88642e346389874251b5a1eded6e3=1517973487,1519619536,1519715701,1519727668; __utma=1.1764713070.1516242744.1519715704.1519727671.7; __utmt=1; __utmb=1.1.10.1519727671; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1519727795",
     }
-
+    # 在粉丝页面用的header
+    my_header2 = {
+        "Host": "xueqiu.com",
+        "Connection": "keep-alive",
+        "Accept": "application/json, text/plain, */*",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36",
+        "Referer": "https://xueqiu.com/u/5762889842",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Cookie": "device_id=288387fa2efe85a6e006edc54a5eda57; s=fp1157htg6; bid=b7785bba313ecf158609498a2d9f49bf_jcjuxw50; __utmz=1.1516242744.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); remember=1; remember.sig=K4F3faYzmVuqC0iXIERCQf55g2Y; xq_a_token=aae1db46abe1e1ed71f6fea88fde2ed306c64063; xq_a_token.sig=rHnC-h2pvBo_iFv1RBKNQsaUc6U; xq_r_token=8ef08c78aefc61aa9394f163640e68c81d338b99; xq_r_token.sig=tDqW3wGLsJBSypal5tKrNYGkbAw; xq_is_login=1; xq_is_login.sig=J3LxgPVPUzbBg3Kee_PquUfih7Q; u=6507446955; u.sig=cSuuCuFU_r2EmTKvwOFMewzKcyI; aliyungf_tc=AQAAAPnKBHOEaw4AomIYdLQ43IMpgjBu; __utmc=1; Hm_lvt_1db88642e346389874251b5a1eded6e3=1517973487,1519619536,1519715701,1519727668; __utma=1.1764713070.1516242744.1519715704.1519727671.7; __utmt=1; __utmb=1.1.10.1519727671; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1519727673",
+    }
     def start_requests(self):
         for url in XueqiuSpider.start_urls:
             yield scrapy.Request(
                                 url=url,
                                 headers=XueqiuSpider.my_header,
                                 callback=self.parse,
-                                meta={'user_id':7675636633}
+                                meta={'user_id':XueqiuSpider.first_user_id}
                                 )
 
     def parse(self, response):
@@ -51,14 +61,13 @@ class XueqiuSpider(scrapy.Spider):
                                follow_num=follow_num, fans_num=fans_num)
             XueqiuSpider.id_queue.append(a_user)
 
-
         if current_page >= page_num: # 当前用户的所有关注页面已经收集完毕
             #开始收集该用户粉丝页面的用户
             url_str = "https://xueqiu.com/friendships/followers.json?uid={user_id}&pageNo=1"
             url = url_str.format(user_id=current_id)
             yield scrapy.Request(
                 url=url,
-                headers=XueqiuSpider.my_header,
+                headers=XueqiuSpider.my_header2,
                 callback=self.parse2, # 收集所有的粉丝
                 meta={'user_id': current_id}
             )
@@ -123,7 +132,7 @@ class XueqiuSpider(scrapy.Spider):
 
             yield scrapy.Request(
                 url=next_url,
-                headers=XueqiuSpider.my_header,
+                headers=XueqiuSpider.my_header2,
                 callback=self.parse2,
                 meta={'user_id': current_id}  # 仍然是当前用户的id
             )
